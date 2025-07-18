c926236fc91cfed3d90277f0b5d95ae28cdbc3fa
Courage Bernard <couragebernard77@gmail.com>
2025-07-18 13:22:49 +0100

    basic auth endpoints

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,6 +2,8 @@ import mongoose from 'mongoose';
 import app from './app';
 
 const PORT = process.env.PORT || 5000;
+
+console.log(process.env.MONGODB_URI)
 const MONGODB_URI = process.env.MONGODB_URI || '';
 
 mongoose.connect(MONGODB_URI)
